a9ecf90b54f6014e2d146b34f550d3ff81496813
Thilak A/L Sundaram <thilak014@gmail.com>
2024-05-12 21:48:14 +0800

    Added new password

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { AuthModule } from './auth/auth.module';
       port: 3306,
       username: 'root',
       password: '1234',
-      database: 'sympspace',
+      database: 'Symspace1234@',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
